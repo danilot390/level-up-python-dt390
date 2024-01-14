ec2787e8677610a6df9cfc9d87e062d4561eee5f
danilot390 <71987540+danilot390@users.noreply.github.com>
2024-01-14 14:26:33 +0000

    palindromes

--- src/02 Identify a Palindrome/palindrome.py
+++ src/02 Identify a Palindrome/palindrome.py
@@ -2,11 +2,16 @@ import re
 
 def is_palindrome(phrase):
     forwards = ''.join(re.findall(r'[a-z]+', phrase.lower()))
-    backwards = forwards[::-1]
-    return forwards == backwards
+    longWords = len(forwards)
+    for i in range(int(longWords/2)):
+        if forwards[i] != forwards[longWords-i-1]:
+            return False 
+    return True
 
 
 # commands used in solution video for reference
 if __name__ == '__main__':
     print(is_palindrome('hello world'))  # false
     print(is_palindrome("Go hang a salami, I'm a lasagna hog."))  # true
+    print(is_palindrome("Sit on a potato pan, Otis."))
+    print(is_palindrome("Moby Dick"))
